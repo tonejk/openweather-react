8a7591920bc1ce9dd4d43c32b66a2cf1831b1259
tonejk <>
2020-10-22 12:52:06 +0300

    installed axios and created api call to get weather data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+const axios = require('axios').default;
+
+const API_KEY = "e253a95cf981ba68d074c93288f773e7";
 
 function App() {
+    const [weather, setWeather] = useState({});
+
+    useEffect(() => {
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Oulu,fi&units=metric&appid=${API_KEY}`)
+        .then(function (data) {
+          setWeather(data.data);
+          console.log(data.data);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+    }, []); //  Run once on load
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+            <p>OpenWeather</p>
+        </header>
+        <div>
+        <li>{weather.main && weather.main.temp}&deg;C</li>
+        <li>{weather.weather && weather.weather[0].main}</li>
+        <li>{weather.name}</li>
+        </div>
     </div>
   );
 }
