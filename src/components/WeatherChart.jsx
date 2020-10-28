b655c92df11083d5a0e96dbe954fd1c1e27c84c6
tonejk <>
2020-10-28 13:24:39 +0200

    added feature to change temp unit

--- src/components/WeatherChart.jsx
+++ src/components/WeatherChart.jsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Style.css";
 
 const WeatherChart = ({ city, country, tempC, tempF, description }) => {
+    const [temp, setTemp] = useState();
+    
+    useEffect(() => {
+        setTemp(tempC)
+    }, [tempC])
+    
+    const ConvertTemp = () => { 
+        if(temp === tempF) {
+            setTemp(tempC);
+        } else if (temp === tempC) {
+            setTemp(tempF);
+        }
+    }
+
     return (
         <div>
+        <button onClick={ConvertTemp}>Change unit</button>
         {city && country ? 
             <div className="chart">
                 {<p>{city}, {country}</p>}
-                {tempC && tempF ? <p>{tempC.toFixed(1)} &deg;C</p> : <p>{tempF.toFixed(1)} &deg;F</p>}
+                {temp ? temp.toFixed(1) : null}
                 {<p>{description}</p>}
             </div>
             : null}
+            {console.log(temp)}
         </div>
     )
 }
