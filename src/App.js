8e87fc308e1155c1a00555d0d71e0aa06598dca1
tonejk <45199872+tonejk@users.noreply.github.com>
2022-12-08 09:52:42 +0000

    changed title

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ function App() {
   return (
     <div className="App">
         <header className="App-header">
-            <p>Urban Weather</p>
+            <p>Weather App</p>
         </header>
         <div>
         <WeatherForm 
